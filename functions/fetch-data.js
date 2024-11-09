21ab18c7a3e29d9b479173272f5fdad56ddead63
Ahmed khaled <155212227+KEPV18@users.noreply.github.com>
2024-11-09 03:15:30 +0200

    Update fetch-data.js

--- functions/fetch-data.js
+++ functions/fetch-data.js
@@ -4,15 +4,15 @@ const sdk = require('node-appwrite');
 // إعداد الاتصال بـ Google Sheets API
 const sheets = google.sheets('v4');
 
-const apiKey = process.env.GOOGLE_API_KEY; // احفظ API Key كمتغير بيئي
-const spreadsheetId = '1yhnXFDyMyCwFP0ttUKG_h9Egw4lrKaJ0HMUR40fQRiY'; // ضع هنا ID الخاص بجوجل شيت
-const range = 'tracking(M)!A1:Z'; // النطاق الذي يحتوي على البيانات
+const apiKey = process.env.GOOGLE_API_KEY;
+const spreadsheetId = '1yhnXFDyMyCwFP0ttUKG_h9Egw4lrKaJ0HMUR40fQRiY';
+const range = 'tracking(M)!A1:Z';
 
 // إعداد اتصال بـ Appwrite
 const client = new sdk.Client();
-client.setEndpoint('https://cloud.appwrite.io/v1') // أدخل عنوان الـ API الخاص بـ Appwrite
-  .setProject('672ea045003d944c6ef4')          // أدخل الـ Project ID الخاص بك
-  .setKey(process.env.APPWRITE_API_KEY);  // أدخل الـ API Key الخاص بـ Appwrite
+client.setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject('672ea045003d944c6ef4')
+  .setKey(process.env.APPWRITE_API_KEY);
 
 const database = new sdk.Database(client);
 
@@ -30,14 +30,14 @@ async function fetchGoogleSheetData() {
 async function storeDataInAppwrite(data) {
     for (let row of data) {
         const document = {
-            Device: row[0],        // ID من جوجل شيت
-            Team: row[1],      // الاسم
-            email: row[2],     // البريد الإلكتروني
+            Device: row[0],
+            Team: row[1],
+            email: row[2],
             Name: row[4],
             Countforallqueues: row[5],
             Quality: row[6],
             Countforeachqueue: row[7],
-            QualityperDay: row[8],     // الهاتف
+            QualityperDay: row[8],
             Countpertoday: row[9],
             LastSubmission: row[10],
             lasttask: row[11],
